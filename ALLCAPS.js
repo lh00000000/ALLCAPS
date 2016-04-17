b84dbfa835cecca27c3b0ace7e7334b0fdc8d9a7
Luming Hao <lh00000000@gmail.com>
2016-04-17 14:41:22 -0400

    A BIT MORE CAPS

--- ALLCAPS.js
+++ ALLCAPS.js
@@ -1,4 +1,4 @@
-var css = document.createElement("style")
-css.type = "text/css"
-css.innerHTML = "* { text-transform: uppercase; font-size: 48px; }"
-document.body.appendChild(css)
+var CSS = document.createElement("style")
+CSS.type = "text/css"
+CSS.innerHTML = "* { text-transform: uppercase; font-size: 48px; }"
+document.body.appendChild(CSS)
